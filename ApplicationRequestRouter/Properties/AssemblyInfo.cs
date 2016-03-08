e6b41862d3394aaa3133f4106019f29dca444658
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2016-03-08 16:15:36 +1100

    Added nuget pack file.

--- ApplicationRequestRouter/Properties/AssemblyInfo.cs
+++ ApplicationRequestRouter/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ApplicationRequestRouter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Application Request Routing Middleware")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ApplicationRequestRouter")]
